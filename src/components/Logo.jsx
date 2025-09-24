2d00c20e51d85aa5dccbe22adaa5cd770ea5a753
mohamadmokarram <mokarram.mohamad@gmail.com>
2025-09-24 23:55:22 +0330

    align anchor tag

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,9 +1,9 @@
 export default function Logo({ title, onClickMenu }) {
   return (
-    <div className="w-full flex justify-between md:block md:w-1/5">
+    <div className="w-full flex justify-between items-center md:block md:w-1/5">
       <a
         href="#home"
-        className="text-white text-2xl py-4 pl-4 font-poppins font-bold capitalize">
+        className="inline-block text-white text-2xl py-4 pl-4 font-poppins font-bold capitalize">
         {title}
       </a>
       {/* hamburger menuIcon */}
