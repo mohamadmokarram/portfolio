8dbe01be7e2b0aeaa02e40fc117e55b0dc1e45ee
mohamadmokarram <mokarram.mohamad@gmail.com>
2025-09-26 20:22:08 +0330

    replacing header and about section photo with less size photo

--- src/components/HeaderPhoto.jsx
+++ src/components/HeaderPhoto.jsx
@@ -1,4 +1,4 @@
-import myImg from "../assets/mokaram.png";
+import myImg from "../assets/mokaram.webp";
 export default function HeaderPhoto({ isMobile }) {
   return (
     <div className="w-full pointer-events-none relative md:w-3/5 overflow-hidden flex justify-center items-center">
